a230802b9510900bf470372b04f3094daa454a7b
Static Builder <innovatewithstatic@gmail.com>
2024-10-30 11:11:49 +0300

    HomeScreen Home, Stats and floating icons set up

--- finance_tracker_app/lib/screens/home/views/home_screen.dart
+++ finance_tracker_app/lib/screens/home/views/home_screen.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 class HomeScreen extends StatelessWidget {
@@ -5,6 +6,32 @@ class HomeScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      appBar: AppBar(),
+      bottomNavigationBar: ClipRRect(
+        borderRadius: const BorderRadius.vertical(
+          top: Radius.circular(30)
+        ),
+        child: BottomNavigationBar(              
+          backgroundColor: Colors.white,
+          showSelectedLabels: false,
+          showUnselectedLabels: false,
+          elevation: 3,
+          items: const [
+            BottomNavigationBarItem(
+              icon: Icon(CupertinoIcons.home),
+              label: 'Home'
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(CupertinoIcons.graph_square_fill),
+              label: 'Stats'
+            ),
+          ]
+
+        ),
+      ),
+      floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
+      floatingActionButton: FloatingActionButton(onPressed: () {})
+    );
   }
 }
