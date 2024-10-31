2a28cd0f6a27c077db5f059b935da69c19ea8c2f
Static Builder <innovatewithstatic@gmail.com>
2024-10-31 21:16:00 +0300

    Connected the app to Firebase and fixed all errors with lauching Flutter

--- finance_tracker_app/lib/main.dart
+++ finance_tracker_app/lib/main.dart
@@ -1,7 +1,17 @@
+import 'package:bloc/bloc.dart';
 import 'package:finance_tracker_app/app.dart';
+import 'package:finance_tracker_app/firebase_options.dart';
+import 'package:finance_tracker_app/simple_bloc_observer.dart';
 import 'package:flutter/material.dart';
+import 'package:firebase_core/firebase_core.dart';
 
-void main() {
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    //options: DefaultFirebaseOptions.currentPlatform,
+);
+ Bloc.observer = SimpleBlocObserver();
   runApp(const MyApp());
 }
 
