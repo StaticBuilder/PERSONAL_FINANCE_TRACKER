d93ebf55b6cc60afddd32bfbcb0486448db76383
Static Builder <innovatewithstatic@gmail.com>
2024-10-31 19:27:08 +0300

    ADD EXPENSE.dart file PART 1(1:20:47)

--- finance_tracker_app/lib/screens/add_expense/views/add_expense.dart
+++ finance_tracker_app/lib/screens/add_expense/views/add_expense.dart
@@ -1,12 +1,347 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:intl/intl.dart';
+import 'package:flutter_colorpicker/flutter_colorpicker.dart'; // Import the color picker package
 
-class AddExpense extends StatelessWidget {
+class AddExpense extends StatefulWidget {
   const AddExpense({super.key});
 
   @override
+  State<AddExpense> createState() => _AddExpenseState();
+}
+
+class _AddExpenseState extends State<AddExpense> {
+  bool isExpended = false;
+  String iconSelected = '';
+  Color categoryColor = Colors.white;
+
+  TextEditingController expenseController = TextEditingController();
+  TextEditingController categoryController = TextEditingController();
+  TextEditingController dateController = TextEditingController();
+  //TextEditingController categoryIconController = TextEditingController(); // Added if required
+  DateTime selectDate = DateTime.now();
+
+  List<String> myCategoriesIcons = [
+    'entertainment',
+    'food',
+    'home',
+    'pet',
+    'shopping',
+    'tech',
+    'travel'
+  ];
+
+  @override
+  void initState() {
+    dateController.text = DateFormat('dd/MM/yyyy').format(DateTime.now());
+    super.initState();
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(),
+    return GestureDetector(
+      onTap: () => FocusScope.of(context).unfocus(),
+      child: Scaffold(
+        backgroundColor: Theme.of(context).colorScheme.surface,
+        appBar: AppBar(
+          backgroundColor: Theme.of(context).colorScheme.surface,
+        ),
+        body: Padding(
+          padding: const EdgeInsets.all(16.0),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              const Text(
+                "Add Expenses",
+                style: TextStyle(fontSize: 22, fontWeight: FontWeight.w500),
+              ),
+              const SizedBox(height: 16),
+              SizedBox(
+                width: MediaQuery.of(context).size.width * 0.7,
+                child: TextFormField(
+                  controller: expenseController,
+                  textAlignVertical: TextAlignVertical.center,
+                  decoration: InputDecoration(
+                    filled: true,
+                    fillColor: Colors.white,
+                    prefixIcon: const Icon(
+                      FontAwesomeIcons.dollarSign,
+                      size: 16,
+                      color: Colors.grey,
+                    ),
+                    border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(30),
+                      borderSide: BorderSide.none,
+                    ),
+                  ),
+                ),
+              ),
+              const SizedBox(height: 32),
+              TextFormField( 
+                controller: categoryController,
+                textAlignVertical: TextAlignVertical.center,
+                readOnly: true,
+                onTap: () {},
+                decoration: InputDecoration(
+                  filled: true,
+                  fillColor: Colors.white,
+                  prefixIcon: const Icon(
+                    FontAwesomeIcons.list,
+                    size: 16,
+                    color: Colors.grey,
+                  ),
+                  suffixIcon: IconButton(
+                    onPressed: () {
+                      showDialog(
+                        context: context,
+                        builder: (ctxt) {
+                          bool isExpanded = false;
+                          String iconSelected = '';
+                          Color categoryColor = Colors.white;
+                          return StatefulBuilder(
+                            builder: (context, setState) {
+                              return AlertDialog(
+                                title: const Text('Create a Category'),
+                                content: SizedBox(
+                                  width: MediaQuery.of(context).size.width,
+                                  child: Column(
+                                    mainAxisSize: MainAxisSize.min,
+                                    children: [
+                                      TextFormField(
+                                        onTap: () {
+                                          setState(() {
+                                            if (isExpanded) isExpanded = false;  // Collapse if expanded
+                                          });
+                                        },
+                                        textAlignVertical: TextAlignVertical.center,
+                                        decoration: InputDecoration(
+                                          isDense: true,
+                                          filled: true,
+                                          fillColor: Colors.white,
+                                          hintText: 'Name',
+                                          border: OutlineInputBorder(
+                                            borderRadius: BorderRadius.circular(12),
+                                            borderSide: BorderSide.none,
+                                          ),
+                                        ),
+                                      ),
+                                      const SizedBox(height: 16),
+                                      TextFormField(
+                                        //controller: categoryIconController,
+                                        onTap: () {
+                                          setState(() {
+                                            isExpanded = !isExpanded;
+                                          });
+                                        },
+                                        textAlignVertical: TextAlignVertical.center,
+                                        readOnly: true,
+                                        decoration: InputDecoration(
+                                          isDense: true,
+                                          filled: true,
+                                          suffixIcon: const Icon(
+                                            CupertinoIcons.chevron_down,
+                                            size: 20,
+                                          ),
+                                          fillColor: Colors.white,
+                                          hintText: 'Icon',
+                                          border: OutlineInputBorder(borderRadius: 
+                                          isExpanded 
+                                          ? const BorderRadius.vertical(top: Radius.circular(12)) 
+                                          : BorderRadius.circular(12), borderSide: BorderSide.none
+                                          ),
+                                        ),
+                                      ),
+                                      isExpanded
+                                        ? Container(
+                                            width: MediaQuery.of(context).size.width,
+                                            height: 200,
+                                            decoration: const BoxDecoration(color: Colors.white, borderRadius: BorderRadius.vertical(bottom: Radius.circular(12))),
+                                            child: Padding(
+                                              padding: const EdgeInsets.all(8.0),
+                                              child: GridView.builder(
+                                                gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(crossAxisCount: 3, mainAxisSpacing: 5, crossAxisSpacing: 5),
+                                                itemCount: myCategoriesIcons.length,
+                                                itemBuilder: (context, int i) {
+                                                  return GestureDetector(
+                                                    onTap: () {
+                                                      setState(() {
+                                                        iconSelected = myCategoriesIcons[i];
+                                                      });
+                                                    },
+                                                    child: Container(
+                                                      width: 50,
+                                                      height: 50,
+                                                      decoration: BoxDecoration(
+                                                          border: Border.all(width: 3, 
+                                                          color: iconSelected == myCategoriesIcons[i] 
+                                                          ? Colors.green 
+                                                          : Colors.grey), borderRadius: BorderRadius.circular(12), image: DecorationImage(image: AssetImage('assets/${myCategoriesIcons[i]}.png'))),
+                                                    ),
+                                                  );
+                                                }
+                                              ),
+                                            ),
+                                          )
+                                        : Container(),
+                                      const SizedBox( height: 16,),
+                                      TextFormField(
+                                        onTap: () {
+                                          showDialog(
+                                            context: context,
+                                            builder: (ctx2) {
+                                              return AlertDialog(
+                                                content: Column(
+                                                  mainAxisSize: MainAxisSize.min,
+                                                  children: [
+                                                    ColorPicker(
+                                                      pickerColor: categoryColor,
+                                                      onColorChanged: (value) {
+                                                        setState(() {
+                                                          categoryColor = value;
+                                                        });
+                                                      },
+                                                    ),
+                                                    SizedBox(
+                                                      width: double.infinity,
+                                                      height: 50,
+                                                      child: TextButton(
+                                                        onPressed: () {
+                                                          Navigator.pop(ctx2);
+                                                        },
+                                                        style: TextButton.styleFrom(
+                                                            backgroundColor: Colors.black,
+                                                            shape: RoundedRectangleBorder(
+                                                                borderRadius:
+                                                                    BorderRadius.circular(
+                                                                        12))),
+                                                        child: const Text(
+                                                          'Save Color',
+                                                          style: TextStyle(
+                                                              fontSize: 22,
+                                                              color: Colors.white),
+                                                        ),
+                                                      ),
+                                                    ),
+                                                  ],
+                                                ),
+                                              );
+                                            },
+                                          );
+                                        },
+                                        textAlignVertical: TextAlignVertical.center,
+                                        readOnly: true,
+                                        decoration: InputDecoration(
+                                          isDense: true,
+                                          filled: true,
+                                          fillColor: categoryColor,
+                                          hintText: 'Color',
+                                          border: OutlineInputBorder(
+                                              borderRadius: BorderRadius.circular(12),
+                                              borderSide: BorderSide.none),
+                                        ),
+                                      ),
+                                      const SizedBox(height: 16),
+                                      SizedBox(
+                                        width: double.infinity,
+                                        height: kToolbarHeight,
+                                        child: TextButton(
+                                          onPressed: () {
+                                            Navigator.pop(context);
+                                          },
+                                          style: TextButton.styleFrom(
+                                            backgroundColor: Colors.black,
+                                            shape: RoundedRectangleBorder(
+                                              borderRadius: BorderRadius.circular(12),
+                                            ),
+                                          ),
+                                          child: const Text(
+                                            'Save',
+                                            style: TextStyle(
+                                              fontSize: 22,
+                                              color: Colors.white,
+                                            ),
+                                          ),
+                                        ),
+                                      ),
+                                    ],
+                                  ),
+                                ),
+                              );
+                            },
+                          );
+                        },
+                      );
+                    },
+                    icon: const Icon(Icons.add),
+                  ),
+                  hintText: 'Category',
+                  border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(12),
+                      borderSide: BorderSide.none,
+                    ),
+                ),
+              ),
+              const SizedBox(height: 16),
+              TextFormField( 
+                controller: dateController,
+                textAlignVertical: TextAlignVertical.center,
+                readOnly: true,
+                onTap: () async {
+                          DateTime? newDate = await showDatePicker(
+                            context: context, 
+                            initialDate: selectDate, //DateTime.now(), //expense.date, 
+                            firstDate: DateTime.now(), 
+                            lastDate: DateTime.now().add(const Duration(days: 365)));
+
+                          if (newDate != null) {
+                            setState(() {
+                              dateController.text = DateFormat('dd/MM/yyyy').format(newDate);
+                              selectDate = newDate;
+                              //expense.date = newDate;
+                            });
+                          }
+                        },
+                decoration: InputDecoration(
+                  filled: true,
+                  fillColor: Colors.white,
+                  prefixIcon: const Icon(
+                    FontAwesomeIcons.clock,
+                    size: 16,
+                    color: Colors.grey,
+                  ),
+                  hintText: 'Date',
+                  border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(12),
+                      borderSide: BorderSide.none,
+                  ),
+                ),
+              ),
+              const SizedBox(height: 32),
+              SizedBox(
+                width: double.infinity,
+                height: kToolbarHeight,
+                child: TextButton(
+                  onPressed: () {  },
+                  style: TextButton.styleFrom(
+                    backgroundColor: Colors.black,
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(12),
+                    ),
+                  ),
+                  child: const Text(
+                    'Save',
+                    style: TextStyle(
+                      fontSize: 22,
+                      color: Colors.white,
+                    ),
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
     );
   }
 }
